c65eb94f973c655a8e122c81637d266004d5af5d
Ravi Kumar Gupta <babaravibbb@gmail.com>
2023-08-18 23:37:46 +0530

    first commit

---
+++ src/main/java/com/example/githubdemoapp/GitHubDemoAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubdemoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubDemoAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitHubDemoAppApplication.class, args);
+    }
+
+}
